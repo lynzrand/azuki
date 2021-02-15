a43fecb49898943e6c7ab15517a3014f7d6698a3
Rynco Maekawa <lynzrand@outlook.com>
2021-02-15 16:51:34 +0800

    feat: prepare for vm impl

--- crates/vm/src/lib.rs
+++ crates/vm/src/lib.rs
@@ -1,1 +1,17 @@
+use std::collections::HashMap;
 
+use azuki_tac::{OpRef, Program};
+use smol_str::SmolStr;
+
+pub struct Vm<'a> {
+    program: &'a Program,
+    stack: Vec<Frame>,
+}
+
+struct Frame {
+    func: SmolStr,
+    instruction: OpRef,
+    vars: HashMap<u32, i64>,
+}
+
+impl Vm<'_> {}
