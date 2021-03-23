b9ecd0334ba3fe569507a6d4cca2c03348a28459
Rynco Maekawa <lynzrand@outlook.com>
2021-03-23 22:58:45 +0800

    feat: more parser impl

--- crates/tac/src/parser/implementation.rs
+++ crates/tac/src/parser/implementation.rs
@@ -107,12 +107,44 @@ impl ParseError {
         }
     }
 
-    pub fn expect_span(reason: Cow<'static, str>, span: lexpr::datum::Span) -> Self {
-        Self::spanned(Expect(reason), span)
+    pub fn expect_span(reason: impl Into<Cow<'static, str>>, span: lexpr::datum::Span) -> Self {
+        Self::spanned(Expect(reason.into()), span)
     }
 
-    pub fn expect_pos(reason: Cow<'static, str>, pos: lexpr::parse::Position) -> Self {
-        Self::position(Expect(reason), pos)
+    pub fn expect_pos(reason: impl Into<Cow<'static, str>>, pos: lexpr::parse::Position) -> Self {
+        Self::position(Expect(reason.into()), pos)
+    }
+
+    pub fn expect(reason: impl Into<Cow<'static, str>>) -> Self {
+        Self::new(Expect(reason.into()))
+    }
+}
+
+trait WithPosition {
+    fn with_position(self, at: Position) -> Self;
+    fn with_position_if_none(self, at: Position) -> Self;
+}
+
+impl WithPosition for ParseError {
+    fn with_position(mut self, at: Position) -> Self {
+        self.at = at;
+        self
+    }
+
+    fn with_position_if_none(mut self, at: Position) -> Self {
+        if let Position::None = self.at {
+            self.at = at
+        }
+        self
+    }
+}
+impl<T> WithPosition for Result<T, ParseError> {
+    fn with_position(self, at: Position) -> Self {
+        self.map_err(|e| e.with_position(at))
+    }
+
+    fn with_position_if_none(self, at: Position) -> Self {
+        self.map_err(|e| e.with_position_if_none(at))
     }
 }
 
@@ -157,10 +189,7 @@ fn parsed_named_type(val: LRef<'_>) -> Result<Ty, ParseError> {
     };
     let len = &name[1..];
     let size = u8::from_str(len).map_err(|e| {
-        ParseError::expect_span(
-            format!("a valid number width, got {}", e).into(),
-            val.span(),
-        )
+        ParseError::expect_span(format!("a valid number width, got {}", e), val.span())
     })?;
     Ok(Ty::Numeric(NumericTy {
         kind: ty_kind,
@@ -180,12 +209,9 @@ fn parse_bb_id(val: LRef<'_>, ctx: &mut VariableNamingCtx) -> Result<BBId, Parse
     let val = val
         .as_name()
         .and_then(|n| n.starts_with("bb").then(|| u32::from_str(&n[2..])))
-        .ok_or_else(|| ParseError::expect_span("a basic block ID number".into(), val.span()))?
+        .ok_or_else(|| ParseError::expect_span("a basic block ID number", val.span()))?
         .map_err(|e| {
-            ParseError::expect_span(
-                format!("basic block ID number, got {}", e).into(),
-                val.span(),
-            )
+            ParseError::expect_span(format!("basic block ID number, got {}", e), val.span())
         })?;
     Ok(ctx.declared_bb(val))
 }
@@ -194,44 +220,167 @@ fn parse_inst_id(val: LRef, ctx: &mut VariableNamingCtx) -> Result<InstId, Parse
     let id = val
         .as_name()
         .and_then(|x| x.starts_with('%').then(|| usize::from_str(&x[1..])))
-        .ok_or_else(|| ParseError::expect_span("instruction ID number".into(), val.span()))?
+        .ok_or_else(|| ParseError::expect_span("instruction ID number", val.span()))?
         .map_err(|e| {
-            ParseError::expect_span(
-                format!("instruction ID number, got {}", e).into(),
-                val.span(),
-            )
+            ParseError::expect_span(format!("instruction ID number, got {}", e), val.span())
         })?;
 
     Ok(ctx.declared_var(id))
+}
+
+fn parse_value(val: LRef<'_>, ctx: &mut VariableNamingCtx) -> Result<Value, ParseError> {
+    if let Some(v) = val.as_i64() {
+        Ok(Value::Imm(v))
+    } else {
+        parse_inst_id(val, ctx).map(|x| x.into())
+    }
+}
+
+fn parse_binary_inst_rest(
+    mut val_iter: ListIter,
+    ctx: &mut VariableNamingCtx,
+    op: BinaryOp,
+) -> Result<InstKind, ParseError> {
+    let lhs = val_iter
+        .next()
+        .ok_or_else(|| ParseError::expect("left hand side value of binary op"))?;
+    let lhs = parse_value(lhs, ctx)?;
+
+    let rhs = val_iter
+        .next()
+        .ok_or_else(|| ParseError::expect("right hand side value of binary op"))?;
+    let rhs = parse_value(rhs, ctx)?;
+
+    Ok(InstKind::Binary(BinaryInst { op, lhs, rhs }))
+}
+
+fn parse_param_rest(mut val_iter: ListIter) -> Result<InstKind, ParseError> {
+    let next = val_iter
+        .next()
+        .ok_or_else(|| ParseError::expect("an integer"))?;
+    let next = next
+        .as_u64()
+        .ok_or_else(|| ParseError::expect_span("an integer", next.span()))?;
+    Ok(InstKind::Param(next as usize))
+}
+
+fn parse_call_rest(
+    mut val_iter: ListIter,
+    ctx: &mut VariableNamingCtx,
+) -> Result<InstKind, ParseError> {
+    let name = val_iter
+        .next()
+        .ok_or_else(|| ParseError::expect("Function name"))?;
+    let name = name.as_name().ok_or_else(|| {
+        ParseError::expect("Function name should be a symbol or string")
+            .with_position(Position::Span(name.span()))
+    })?;
+
+    let param_list = val_iter
+        .next()
+        .ok_or_else(|| ParseError::expect("Function param list"))?;
+    let param_list = param_list.list_iter().ok_or_else(|| {
+        ParseError::expect_span("Function param list should be a list", param_list.span())
+    })?;
+
+    let params = param_list
+        .map(|x| parse_value(x, ctx))
+        .collect::<Result<Vec<_>, _>>()?;
+
+    Ok(InstKind::FunctionCall(FunctionCall {
+        name: name.into(),
+        params,
+    }))
+}
+
+fn parse_phi_rest(val_iter: ListIter, ctx: &mut VariableNamingCtx) -> Result<InstKind, ParseError> {
+    let phi = val_iter
+        .map(|x| {
+            let (bb, id) = x
+                .as_pair()
+                .ok_or_else(|| ParseError::expect_span("phi item", x.span()))?;
+            let bb = parse_bb_id(bb, ctx)?;
+            let id = parse_inst_id(id, ctx)?;
+            Ok((bb, id))
+        })
+        .collect::<Result<BTreeMap<_, _>, _>>()?;
+
+    Ok(InstKind::Phi(phi))
 }
 
 fn parse_inst_kind(
     mut val_iter: ListIter,
     ctx: &mut VariableNamingCtx,
 ) -> Result<InstKind, ParseError> {
-    todo!()
+    let name = val_iter
+        .next()
+        .ok_or_else(|| ParseError::expect("instruction name or immediate"))?;
+    let name_span = name.span();
+
+    // Early return - number case
+    if let Some(n) = name.as_i64() {
+        return Ok(InstKind::Assign(Value::Imm(n)));
+    }
+
+    let name = name
+        .as_name()
+        .ok_or_else(|| ParseError::expect("instruction name"))?;
+
+    match name {
+        "add" => parse_binary_inst_rest(val_iter, ctx, BinaryOp::Add),
+        "sub" => parse_binary_inst_rest(val_iter, ctx, BinaryOp::Sub),
+        "mul" => parse_binary_inst_rest(val_iter, ctx, BinaryOp::Mul),
+        "div" => parse_binary_inst_rest(val_iter, ctx, BinaryOp::Div),
+        "eq" => parse_binary_inst_rest(val_iter, ctx, BinaryOp::Eq),
+        "ne" => parse_binary_inst_rest(val_iter, ctx, BinaryOp::Ne),
+        "lt" => parse_binary_inst_rest(val_iter, ctx, BinaryOp::Lt),
+        "gt" => parse_binary_inst_rest(val_iter, ctx, BinaryOp::Gt),
+        "le" => parse_binary_inst_rest(val_iter, ctx, BinaryOp::Le),
+        "ge" => parse_binary_inst_rest(val_iter, ctx, BinaryOp::Ge),
+
+        "param" => parse_param_rest(val_iter),
+
+        "call" => parse_call_rest(val_iter, ctx),
+
+        "phi" => parse_phi_rest(val_iter, ctx),
+
+        n if n.starts_with('%') => {
+            let id = usize::from_str(n)
+                .map_err(|e| ParseError::expect_span(format!("var id, got {}", e), name_span))?;
+            let id = ctx.declared_var(id);
+            Ok(InstKind::Assign(Value::Dest(id)))
+        }
+
+        _ => Err(ParseError::expect_span(
+            "A valid instruction name",
+            name_span,
+        )),
+    }
 }
 
 // (<dest> <ty> <opcode> ...<params>)
 fn parse_inst(val: LRef<'_>, ctx: &mut VariableNamingCtx) -> Result<(), ParseError> {
     let mut inst_iter = val.list_iter().ok_or_else(|| {
-        ParseError::expect_span("An instruction represented as a list".into(), val.span())
+        ParseError::expect_span("An instruction represented as a list", val.span())
     })?;
 
     let inst_id = inst_iter
         .next()
-        .ok_or_else(|| ParseError::expect_pos("instruction ID number".into(), val.span().end()))?;
+        .ok_or_else(|| ParseError::expect_pos("instruction ID number", val.span().end()))?;
     let inst_id = parse_inst_id(inst_id, ctx)?;
 
     let ty = inst_iter
         .next()
-        .ok_or_else(|| ParseError::expect_pos("instruction value type".into(), val.span().end()))?;
+        .ok_or_else(|| ParseError::expect_pos("instruction value type", val.span().end()))?;
     let ty = parse_type(ty)?;
 
-    let kind = parse_inst_kind(inst_iter, ctx)?;
+    let kind = parse_inst_kind(inst_iter, ctx)
+        .with_position_if_none(Position::Position(val.span().end()))?;
 
     let inst = Inst { kind, ty };
     *ctx.func.func.inst_get_mut(inst_id) = inst;
+    ctx.func.put_inst_after_current_place(inst_id);
+
     Ok(())
 }
 
@@ -239,18 +388,19 @@ fn parse_inst(val: LRef<'_>, ctx: &mut VariableNamingCtx) -> Result<(), ParseErr
 fn parse_bb(val: LRef<'_>, ctx: &mut VariableNamingCtx) -> Result<(), ParseError> {
     let mut list = val
         .list_iter()
-        .ok_or_else(|| ParseError::spanned(Expect("Basic block definition".into()), val.span()))?;
+        .ok_or_else(|| ParseError::expect_span("Basic block definition", val.span()))?;
 
-    let id = list.next().ok_or_else(|| {
-        ParseError::expect_pos("the ID of a basic block".into(), val.span().end())
-    })?;
+    let id = list
+        .next()
+        .ok_or_else(|| ParseError::expect_pos("the ID of a basic block", val.span().end()))?;
     let id = parse_bb_id(id, ctx)?;
+    ctx.func.set_current_bb(id);
 
     // parse instruction list
     let instructions = list
         .next()
         .and_then(|x| x.list_iter())
-        .ok_or_else(|| ParseError::expect_pos("a list of instructions".into(), val.span().end()))?;
+        .ok_or_else(|| ParseError::expect_pos("a list of instructions", val.span().end()))?;
     instructions
         .map(|x| parse_inst(x, ctx))
         .collect::<Result<Vec<_>, _>>()?;
@@ -258,6 +408,7 @@ fn parse_bb(val: LRef<'_>, ctx: &mut VariableNamingCtx) -> Result<(), ParseError
     todo!()
 }
 
+// (fn <name> <param> <return> ...<basic-blocks>)
 pub fn parse_function(val: LRef<'_>) -> Result<TacFunc, ParseError> {
     let mut list = val
         .list_iter()
--- resource/fib.tac
+++ resource/fib.tac
@@ -1,21 +1,25 @@
-(fn @fib (i32) i32
+(fn fib (i32) i32
     (bb0 (
         (%0 i32 param 0)
-        (%1 i32 le %0 1))
-        (br (if %1 bb1) bb2))
-    (bb1 ()
-        (return 1))
+        (%1 b32 le %0 1))
+        (brif %1 bb1 bb2))
+    (bb1 (
+        (%7 i32 1))
+        (br bb3))
     (bb2 (
         (%2 i32 sub %0 1)
         (%3 i32 sub %0 2)
-        (%4 i32 call @fib (%2))
-        (%5 i32 call @fib (%3))
+        (%4 i32 call fib (%2))
+        (%5 i32 call fib (%3))
         (%6 i32 add %4 %5))
-        (return %6)))
+        (br bb3))
+    (bb3 (
+        (%7 i32 phi (bb1 %7) (bb2 %6)))
+        (return %7)))
 
-(fn @main () i32 
+(fn main () i32 
     (bb0 (
-        (%0 i32 call @fib (15)))
+        (%0 i32 call fib (15)))
         (return %0)))
 
 (%5678 nil call @intrinsinc:putint (123))
