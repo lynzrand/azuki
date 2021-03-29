0f9cb7c8bb1b0b7d63474876d2fc5fc5a8f4925e
Rynco Maekawa <lynzrand@outlook.com>
2021-03-29 17:17:27 +0800

    fix: merge stuff

--- crates/opt/src/dead_code_eliminator.rs
+++ crates/opt/src/dead_code_eliminator.rs
@@ -30,6 +30,8 @@ impl FunctionOptimizer for DeadCodeEliminator {
         _env: &mut azuki_tac::optimizer::OptimizeEnvironment,
         func: &mut azuki_tac::TacFunc,
     ) {
+        let mut graph = DiGraphMap::new();
+        let mut find_roots = VecDeque::new();
         let _span = debug_span!("dead-code-eliminator", %func.name).entered();
 
         debug!("Constructing reference map");
@@ -41,22 +43,22 @@ impl FunctionOptimizer for DeadCodeEliminator {
         }
         for (_, bb) in func.all_bb_unordered() {
             if let Branch::Return(Some(Value::Dest(idx))) = &bb.branch {
-                self.find_roots.push_back(*idx);
+                find_roots.push_back(*idx);
             } else if let Branch::CondJump {
                 cond: Value::Dest(x),
                 ..
             } = &bb.branch
             {
                 // TODO: Add condition to find root only if it contributes to return value
-                self.find_roots.push_back(*x);
+                find_roots.push_back(*x);
             }
         }
 
         debug!("Finding reachable variables");
         // calculate spanning tree
         let mut retained = HashSet::new();
-        let mut dfs = petgraph::visit::Dfs::empty(&self.graph);
-        while let Some(root) = self.find_roots.pop_front() {
+        let mut dfs = petgraph::visit::Dfs::empty(&graph);
+        while let Some(root) = find_roots.pop_front() {
             dfs.move_to(root);
             retained.insert(root);
             for point in (&mut dfs).iter(&graph) {
@@ -86,10 +88,7 @@ impl FunctionOptimizer for DeadCodeEliminator {
         }
     }
 
-    fn reset(&mut self) {
-        self.graph.clear();
-        self.find_roots.clear();
-    }
+    fn reset(&mut self) {}
 
     fn edits_program(&self) -> bool {
         true
